86bce6d01e8a7dadc9e7563cc9de9f635f11d13b
123456 <user@debian>
2023-12-29 16:20:02 +0800

    first commit

---
+++ init.el
@@ -0,0 +1,49 @@
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+(setq-default tab-width 2)
+(setq-default indent-tabs-mode nil)
+(setq inhibit-startup-message t)
+
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(delete-selection-mode t)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(blink-cursor-mode t)
+(show-paren-mode t)
+(column-number-mode t)
+(set-fringe-style -1)
+(tooltip-mode -1)
+
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
+(setq use-package-always-ensure t)
+
+(use-package better-defaults)
+
+(use-package vertico
+  :init
+  (vertico-mode)
+  (setq vertico-scroll-margin 0)
+  (setq vertico-count 5))
+
+(keymap-set vertico-map "M-TAB" #'minibuffer-compolete)
+
+(use-package meow
+  :init
+  (meow-setup)
+  (meow-global-mode 1))
+
+(use-package rime
+  :custom
+  (default-input-method "rime"))
